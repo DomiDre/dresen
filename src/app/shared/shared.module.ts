e5b57fe8cbe9ad071b1a83625905661ed753710a
DomiDre <homiedomi@gmail.com>
2019-08-28 22:34:42 +0200

    Add firebase

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
+    AngularFireModule,
+    AngularFirestoreModule
   ],
   exports: [
     MaterialModule,
