abe176d295e2e927219b0e40e8bd82b7435c00bb
DomiDre <homiedomi@gmail.com>
2019-11-14 14:03:48 +0100

    Add admin function to edit post

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AddPostComponent } from './add-post/add-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
 import { RemovePostComponent } from './remove-post/remove-post.component';
 import { LoginComponent } from './login/login.component';
 import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
@@ -7,7 +8,8 @@ import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 export const routableComponents = [
 	AddPostComponent,
 	RemovePostComponent,
-	LoginComponent
+	LoginComponent,
+	EditPostComponent
 ];
 
 
@@ -19,6 +21,11 @@ export const routes: Routes = [
 	{
 		path: 'addPost',
 		component: AddPostComponent,
+		canActivate: [AngularFireAuthGuard]
+	},
+	{
+		path: 'editPost',
+		component: EditPostComponent,
 		canActivate: [AngularFireAuthGuard]
 	},
 	{
--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -3,6 +3,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
 import { BlogPost, BlogSnippet } from '@shared/models/blog.models';
 import { BlogService } from '@app/modules/blog/services/blog.service';
+import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +13,8 @@ export class AdminService {
 
   constructor(
     private db: AngularFirestore,
-    private blogService: BlogService
+    private blogService: BlogService,
+    private router: Router
   ) { }
   
   /**
@@ -18,8 +22,9 @@ export class AdminService {
    * @param title Title of Blog Post
    * @param content Markdown String of Post
    */
-  generateSnippet(title: string, content: string, topic: string,  abstract?: string): [BlogPost, BlogSnippet] {
-    const timestamp = Date.now();
+  generateSnippet(title: string, content: string, topic: string, 
+    abstract?: string, timestamp?: number, id?: string): [BlogPost, BlogSnippet] {
+    if (!timestamp) timestamp = Date.now();
     abstract = abstract ? abstract : content.split(' ').slice(0, 42).join(' ');
     const blogPost: BlogPost = {
       title,
@@ -29,7 +34,7 @@ export class AdminService {
     }
 
     const blogSnippet: BlogSnippet = {
-      id: undefined,
+      id,
       abstract,
       title,
       timestamp,
@@ -43,8 +48,12 @@ export class AdminService {
    * Write a blog post to Firebase.
    * New lines are replaced by <br> for storage
    */
-  writeBlogPost(blogPost: BlogPost, blogSnippet: BlogSnippet): Promise<void> {
-
+  async writeBlogPost(blogPost: BlogPost, blogSnippet: BlogSnippet): Promise<void> {
+    if (blogSnippet.id) {
+      console.error(
+        "writeBlogPost() should not be used with already added posts. " +
+        "Use editBlogPost() if id is already defined")
+    }
     const docId = this.db.createId();  
     blogSnippet.id = docId;
 
@@ -65,7 +74,8 @@ export class AdminService {
       this.db.collection('blog_snippets').doc(blogSnippet.topic).ref,
       { content: firebase.firestore.FieldValue.arrayUnion(blogSnippet)}
     )
-    return batch.commit();
+    await batch.commit();
+    this.router.navigate(['/']);
   }
 
   /**
@@ -89,6 +99,59 @@ export class AdminService {
       { content: firebase.firestore.FieldValue.arrayRemove(blogSnippet)}
     );
     return batch.commit();
+  }
+
+  /**
+   * Edit an existing blog post.
+   */
+  async editBlogPost(blogPost: BlogPost, blogSnippet: BlogSnippet): Promise<void> {
+    if (!blogSnippet.id) {
+      console.error("Provided blog snippet without an ID");
+      return
+    }
+    
+    const read_snippets_promises = [];
+
+    read_snippets_promises.push(
+      this.db.firestore.collection('blog_snippets').doc('recent').get()
+    );
+    read_snippets_promises.push(
+      this.db.firestore.collection('blog_snippets').doc(blogSnippet.topic).get()
+    )
+    
+    let [recent_snippets, topic_snippets] = await Promise.all(read_snippets_promises);
+    if (recent_snippets.exists && topic_snippets.exists) {
+      recent_snippets = recent_snippets.data();
+      topic_snippets = topic_snippets.data();
+    } else {
+      console.error("Did not find snippets");
+      return
+    }
+
+    const id_recent = recent_snippets.content.findIndex(element => element.id === blogSnippet.id)
+    const id_topic = topic_snippets.content.findIndex(element => element.id === blogSnippet.id)
+
+    recent_snippets.content[id_recent] = blogSnippet
+    topic_snippets.content[id_topic] = blogSnippet
+
+    blogPost.content = blogPost.content.replace(/(\r\n|\n|\r)/gm,"<br>");
+    blogSnippet.abstract = blogSnippet.abstract.replace(/(\r\n|\n|\r)/gm,"<br>");
+    
+    const batch = this.db.firestore.batch();
+    batch.update(
+      this.db.collection('blog_posts').doc(blogSnippet.id).ref,
+      blogPost
+    );
+    batch.update(
+      this.db.collection('blog_snippets').doc('recent').ref,
+      recent_snippets
+    )
+    batch.update(
+      this.db.collection('blog_snippets').doc(blogSnippet.topic).ref,
+      topic_snippets
+    )
+    await batch.commit();
+    this.router.navigate(['/']);
   }
 }
 
